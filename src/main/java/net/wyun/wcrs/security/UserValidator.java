6b820b71169063846f56d3ecb4611dde4e5671d7
michaelyin <gt8442b@yahoo.com>
2017-04-21 15:50:25 -0400

    user validation file.

---
+++ src/main/java/net/wyun/wcrs/security/UserValidator.java
@@ -0,0 +1,42 @@
+package net.wyun.wcrs.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import net.wyun.wcrs.model.Account;
+
+@Component
+public class UserValidator implements Validator {
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return Account.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+    	Account account = (Account) o;
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "NotEmpty");
+        if (account.getUsername().length() < 6 || account.getUsername().length() > 32) {
+            errors.rejectValue("username", "Size.userForm.username");
+        }
+        if (userService.findByUsername(account.getUsername()) != null) {
+            errors.rejectValue("username", "Duplicate.userForm.username");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "NotEmpty");
+        if (account.getPassword().length() < 8 || account.getPassword().length() > 32) {
+            errors.rejectValue("password", "Size.userForm.password");
+        }
+
+        if (!account.getPasswordConfirm().equals(account.getPassword())) {
+            errors.rejectValue("passwordConfirm", "Diff.userForm.passwordConfirm");
+        }
+    }
+}
